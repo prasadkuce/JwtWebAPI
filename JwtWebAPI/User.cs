fa15be5862ef6e751231b0171d3c7685bf6a359b
prasad <prasadkuce@gmail.com>
2023-05-01 20:32:52 +0530

    Refresh Token

--- JwtWebAPI/User.cs
+++ JwtWebAPI/User.cs
@@ -5,5 +5,9 @@
         public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public string RefreshToken { get; set; }
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
     }
 }
